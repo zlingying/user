de437dc933280a26272d01c881b17c21821aa1b6
zly <877673916@qq.com>
2020-01-13 08:18:59 +0800

    111

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    user
                 </div>
 
                 <div class="links">
